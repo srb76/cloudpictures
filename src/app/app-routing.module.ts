d12231eae163bce0fbfc39e7c988cf04d88617fa
srb76 <bonnersa@oregonstate.edu>
2020-06-26 12:18:00 -0700

    Generated upload page and route, minor color changes to home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'upload',
+    loadChildren: () => import('./upload/upload.module').then( m => m.UploadPageModule)
+  },
 ];
 
 @NgModule({
