a794da03a5d9fb45e5981034bc839c405ab46fe8
srb76 <bonnersa@oregonstate.edu>
2020-06-26 16:44:05 -0700

    Update camera.service.ts
    Added savePhoto, readAsBase64, and convertBlobToBase64 functions to convert photos to a format usable by the Capacitor Filesystem API

--- src/app/services/camera.service.ts
+++ src/app/services/camera.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Plugins, CameraResultType, Capacitor, FilesystemDirectory, CameraPhoto, CameraSource} from '@capacitor/core';
+import { summaryFileName } from '@angular/compiler/src/aot/util';
 
 const {Camera, Filesystem, Storage} = Plugins;
 
@@ -20,12 +21,53 @@ export class CameraService {
       quality: 100
     });
 
+    //save photo
+    const savedImage = await this.savePhoto(newPhoto);
+    this.photos.unshift(savedImage);
+
     //add photo to Photos array
+    /*
     this.photos.unshift({
       filepath: "/photos",
       webviewPath: newPhoto.webPath
     });
+    */
   }
+
+  private async savePhoto(cameraPhoto: CameraPhoto){
+    //convert image to base64
+    const base64Data = await this.readAsBase64(cameraPhoto);
+
+    //write file to directory
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: FilesystemDirectory.Data
+    });
+
+    return {
+      filepath: fileName,
+      webviewPath: cameraPhoto.webPath
+    };
+  }
+
+  private async readAsBase64(cameraPhoto: CameraPhoto){
+    //from: https://ionicframework.com/docs/angular/your-first-app/3-saving-photos
+    const response = await fetch(cameraPhoto.webPath!);
+    const blob = await response.blob();
+
+    return await this.convertBlobToBase64(blob) as string;
+  }
+
+  convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader;
+    reader.onerror = reject;
+    reader.onload = () => {
+        resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
 }
 
 interface Photo{
